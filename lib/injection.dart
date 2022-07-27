8bcac74503b3db34b31e84aaa51eb0cbbd57f875
xavier Quiroz <xavierpolivioquirozbriones@hotmail.com>
2022-07-27 08:00:51 -0500

    envio cambios

---
+++ lib/injection.dart
@@ -0,0 +1,6 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {}
---
+++ lib/ui/Router/FluroRouter.dart
@@ -0,0 +1,9 @@
+import 'package:fluro/fluro.dart';
+
+class Flurorouter {
+  static final FluroRouter router = new FluroRouter();
+
+  static void configureRoutes() {
+    //router.notFoundHandler = Handlers.noFound;
+  }
+}
